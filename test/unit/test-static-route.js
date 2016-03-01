13e7d1bfda21d0f8d31cf5868ab294eaa91aa495
Lucas Holmquist <lholmqui@redhat.com>
2016-03-01 15:03:58 -0500

    addition of tests. RHMAP-3215

---
+++ test/unit/test-static-route.js
@@ -0,0 +1,67 @@
+var assert = require('assert');
+var request = require('supertest');
+var proxyquire = require('proxyquire');
+
+var corsMock = function () {
+    return function (req, res, next) {
+
+        return next();
+    };
+};
+var mbaasApiStub = {
+    mbaasExpress: function () {
+        return {
+            sys: function () {
+                return function (req, res, next) {return next();};
+            },
+            mbaas: function (req, res, next) {
+                return next();
+            },
+            fhmiddleware: function () {
+                return function (req, res, next) {
+                    return next();
+                };
+            },
+            cloud: function () {
+                return function (req, res, next) {return next();};
+            },
+            errorHandler: function () {
+                return function (req, res, next) {return next();};
+            }
+        };
+    },
+    db: function (arg1, arg2) {
+        var listView = {
+            list: [{
+                fields: {
+                    assets: require('../../lib/assets.json')
+                }
+            }]
+        };
+        arg2(null, listView);
+    },
+    '@global': true
+};
+
+var mocks = {
+    'fh-mbaas-api': mbaasApiStub,
+    './lib/bootstrapper': {
+      bootstrapData: function () { return []; }
+    }
+};
+
+
+var app = proxyquire('../../application.js', mocks);
+
+
+exports['test returned months data'] = function (done) {
+  request(app)
+  .get('/assets').expect(200).end(function (err, response) {
+    var body = response.body;
+
+    assert(Array.isArray(body), 'response body should be an array');
+    assert.equal(body[0].key, 0, 'first key is 0');
+    assert.equal(body[0].value, 'Office Chairs', 'first value is Office Chairs');
+    return done();
+  });
+};
