8ca200b93c1756ed1f5eb60256c218969b7560cf
Pavel Šturc <psturc@redhat.com>
2016-07-12 09:01:16 +0200

    Test using local MongoDB, removed grunt --force

--- test/unit/test-static-route.js
+++ test/unit/test-static-route.js
@@ -1,58 +1,6 @@
 var assert = require('assert');
 var request = require('supertest');
-var proxyquire = require('proxyquire');
-
-var corsMock = function () {
-    return function (req, res, next) {
-
-        return next();
-    };
-};
-var mbaasApiStub = {
-    mbaasExpress: function () {
-        return {
-            sys: function () {
-                return function (req, res, next) {return next();};
-            },
-            mbaas: function (req, res, next) {
-                return next();
-            },
-            fhmiddleware: function () {
-                return function (req, res, next) {
-                    return next();
-                };
-            },
-            cloud: function () {
-                return function (req, res, next) {return next();};
-            },
-            errorHandler: function () {
-                return function (req, res, next) {return next();};
-            }
-        };
-    },
-    db: function (arg1, arg2) {
-        var listView = {
-            list: [{
-                fields: {
-                    assets: require('../../lib/assets.json')
-                }
-            }]
-        };
-        arg2(null, listView);
-    },
-    '@global': true
-};
-
-var mocks = {
-    'fh-mbaas-api': mbaasApiStub,
-    './lib/bootstrapper': {
-      bootstrapData: function () { return []; }
-    }
-};
-
-
-var app = proxyquire('../../application.js', mocks);
-
+var app = require('../../application.js');
 
 exports['test returned months data'] = function (done) {
   request(app)
