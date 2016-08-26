81fe0523924f91b015e0f442ee05394c1ebb29cb
witmicko <mogrodni@redhat.com>
2016-08-26 14:53:40 +0100

    test fix

--- test/unit/test-static-route.js
+++ test/unit/test-static-route.js
@@ -6,7 +6,6 @@ exports['test returned months data'] = function (done) {
   request(app)
   .get('/assets').expect(200).end(function (err, response) {
     var body = response.body;
-
     assert(Array.isArray(body), 'response body should be an array');
     assert.equal(body[0].key, 0, 'first key is 0');
     assert.equal(body[0].value, 'Office Chairs', 'first value is Office Chairs');
